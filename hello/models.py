e5f0f3295f6ae88120c3ca6f9277be06f4c3d108
Felipe Abello <pipe@helloguru.io>
2023-01-10 18:41:54 -0800

    update datetime now

--- hello/models.py
+++ hello/models.py
@@ -5,17 +5,17 @@ class Question(models.Model):
     question = models.CharField(max_length=140)
     context = models.TextField(null=True, blank=True)
     answer = models.TextField(max_length=1000, null=True, blank=True)
-    created_at = models.DateTimeField("date created", auto_now_add=True, null=True)
+    created_at = models.DateTimeField("date created", default=datetime.now, null=True)
     ask_count = models.IntegerField(default=1)
     #audio_src_url = models.CharField(default="", max_length=255, null=True, blank=True)
 
 class user_avatars(models.Model):
-    created_at = models.DateTimeField("date created", auto_now_add=True, null=True)
+    created_at = models.DateTimeField("date created", default=datetime.now, null=True)
     avatar_id = models.IntegerField(null=True)
     user_id = models.IntegerField(null=True)
 
 class avatars(models.Model):
-    created_at = models.DateTimeField("date created", auto_now_add=True, null=True)
+    created_at = models.DateTimeField("date created", default=datetime.now, null=True)
     name = models.TextField(null=True, blank=True)
     description = models.TextField(null=True, blank=True)
     visibility = models.TextField(null=True, blank=True)
@@ -23,6 +23,6 @@ class avatars(models.Model):
     status = models.TextField(null=True, blank=True)
 
 class files(models.Model):
-    created_at = models.DateTimeField("date created", auto_now_add=True, null=True)
+    created_at = models.DateTimeField("date created", default=datetime.now, null=True)
     avatar_id = models.IntegerField(null=True)
     file_url = models.CharField(default="", max_length=255, null=True, blank=True)
