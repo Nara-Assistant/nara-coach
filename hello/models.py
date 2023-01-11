38412de9f98f3cab0888ea7fb2b768cb7e9d5c8a
Felipe Abello <pipe@helloguru.io>
2023-01-10 17:07:40 -0800

    Add migrations

--- hello/models.py
+++ hello/models.py
@@ -7,3 +7,21 @@ class Question(models.Model):
     created_at = models.DateTimeField("date created", auto_now_add=True)
     ask_count = models.IntegerField(default=1)
     #audio_src_url = models.CharField(default="", max_length=255, null=True, blank=True)
+
+class user_avatars(models.Model):
+    created_at = models.DateTimeField("date created", auto_now_add=True)
+    avatar_id = models.IntegerField(null=True)
+    user_id = models.IntegerField(null=True)
+
+class avatars(models.Model):
+    created_at = models.DateTimeField("date created", auto_now_add=True)
+    name = models.TextField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    visibility = models.TextField(null=True, blank=True)
+    url_path = models.TextField(null=True, blank=True)
+    status = models.TextField(null=True, blank=True)
+
+class files(models.Model):
+    created_at = models.DateTimeField("date created", auto_now_add=True)
+    avatar_id = models.IntegerField(null=True)
+    file_url = models.CharField(default="", max_length=255, null=True, blank=True)
