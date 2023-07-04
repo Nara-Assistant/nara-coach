c830fa45bc28a245285ed60fe76fa46954e61b49
Jose Biskis <jose.biskis@helloguru.io>
2023-07-03 20:25:18 -0400

    Try Except per file

--- hello/db_embeddings_utils.py
+++ hello/db_embeddings_utils.py
@@ -9,38 +9,43 @@ from urllib.parse import urlparse
 from .dbconnect import emb_conn
 from .models import files as files_model
 import json
+from .notifications import send_notification
 
 def train_db(url, file_id):
-    ts = time.time()
-    a = urlparse(url)
-    filename = f"{file_id}-{ts}"
-    file_module.download_file(url, f"{filename}-{os.path.basename(a.path)}")
-    pdf_text = extract_pdf_text.get_text(f"{filename}-{os.path.basename(a.path)}")
+    try: 
+        ts = time.time()
+        a = urlparse(url)
+        filename = f"{file_id}-{ts}"
+        file_module.download_file(url, f"{filename}-{os.path.basename(a.path)}")
+        pdf_text = extract_pdf_text.get_text(f"{filename}-{os.path.basename(a.path)}")
 
-    # print(pdf_text)
-    with emb_conn:
-        with emb_conn.cursor() as emb_curs:
-            try: 
-                emb_curs.execute(f"delete from hello_file_embeddings where file_id={file_id}")
+        # print(pdf_text)
+        with emb_conn:
+            with emb_conn.cursor() as emb_curs:
+                try: 
+                    emb_curs.execute(f"delete from hello_file_embeddings where file_id={file_id}")
+                except Exception as e:
+                    print(e)
+
+        print("Is here")
+        chunks = tokens_per_string.split_chunks(pdf_text, chunk_size = 200)
+        print(chunks)
+        chunk_array = [(key, chunk) for key, chunk in enumerate(chunks)]
+        for key, chunk in chunk_array:
+            try:  
+                print("Hey first iteration")  
+                response = openai_requests.get_embedding(chunk[0])
+                print("Hey first iteration:after")  
+                dbembeddings.insert_embeddings(response, chunk[0], file_id, key + 1, chunk[1])
             except Exception as e:
-                print(e)
+                chunk_array = [
+                    *chunk_array,
+                    (key, chunk)
+                ]
+            # print((key + 1, len(response)))
+    except Exception as e:
+        send_notification("train_db", "nara-heroku", [("url", url), ("file_id", file_id)])
 
-    print("Is here")
-    chunks = tokens_per_string.split_chunks(pdf_text, chunk_size = 200)
-    print(chunks)
-    chunk_array = [(key, chunk) for key, chunk in enumerate(chunks)]
-    for key, chunk in chunk_array:
-        try:  
-            print("Hey first iteration")  
-            response = openai_requests.get_embedding(chunk[0])
-            print("Hey first iteration:after")  
-            dbembeddings.insert_embeddings(response, chunk[0], file_id, key + 1, chunk[1])
-        except Exception as e:
-            chunk_array = [
-                *chunk_array,
-                (key, chunk)
-            ]
-        # print((key + 1, len(response)))
 
 MAX_TOKENS = 1000
 SEPARATOR = "\n* "
