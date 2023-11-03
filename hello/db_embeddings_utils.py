f5b8f0777b9e0790e4d277b1ea03984d2435f1a1
Jose Biskis <jose.biskis@helloguru.io>
2023-11-02 20:03:06 -0400

    Threshold & Count

--- hello/db_embeddings_utils.py
+++ hello/db_embeddings_utils.py
@@ -84,7 +84,7 @@ def train_db(url, file_id, raw_data = None, avatar_id = None):
 SEPARATOR = "\n* "
 
 def build_prompt(query, files_ids, threshold = 0.5, count = 10):
-    MAX_TOKENS = count * 20
+    MAX_TOKENS = count * 200
     response = openai_requests.get_embedding(query)
     documents_response = dbembeddings.match_documents(response, threshold=threshold, count=count, files_ids=files_ids)
     print(documents_response)
